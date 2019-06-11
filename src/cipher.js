d5b84f5d5696e10b310603e589136c8d09d79eba
Celina Urzúa <celinaurzua@99.200-57-1.bestelclientes.com.mx>
2019-06-11 17:43:18 -0500

    se agregan cambios en cipher.js e index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,18 @@
 window.cipher = {
-  // ... 
-};
+  //formula encriptado cesar  
+  String: caesarShift = (shift) - >
+  result = ''
+i = 0
+while i < length
+c = charCodeAt i
+if c >= 65 and c <= 90
+result += charsUP[(c - 65 + shift) % 26]
+else if c >= 97 and c <= 122
+result += charsDN[(c - 97 + shift) % 26]
+else
+  result += charAt i;
+i++
+result
+
+// ... 
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-console.log(¡Hola mundo!);
+//evento para cifrar boton de encriptado
+('.btn-encrypt').click - >
+shift = ('.shift-l').val() % 26
+text = ('plaintext').val()
+cipher = text.caesarShift shift('ciphertext').val cipher
+return
+
+//evento para boton desencriptado
+('.btn-decrypt').click 
+shift = ('.shift-r').val() % 26
+text = ('ciphertext').val()
+plaintext = text.caesarShift - shift('plaintext').val plaintext
+return
+
+('.shift-r').change - >
+shift = ('.shift-r').val() % 26
+text = ('ciphertext').val()
+plaintext = text.caesarShift - shift
+('plaintext').val plaintext
+return
 
